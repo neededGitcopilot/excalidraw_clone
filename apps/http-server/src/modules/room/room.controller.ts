0ea6da6ef772e763f478bec3eff68958f0fff80f
karan ahir <neededgithub@gmail.com>
2026-02-09 19:26:23 +0530

    add room create controller

--- apps/http-server/src/modules/room/room.controller.ts
+++ apps/http-server/src/modules/room/room.controller.ts
@@ -1,8 +1,12 @@
 
+import prismaClient from "@repo/db/client";
 import { Request, RequestHandler, Response } from "express";
 import asyncHandler from 'express-async-handler';
 
 
 export const createRoom : RequestHandler = asyncHandler(async (req, res) => {
 
+  await prismaClient.default.room.create({
+    data : {adminId: req.user.id}
+  })
 });
