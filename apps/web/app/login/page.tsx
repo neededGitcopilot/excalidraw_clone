582424f47d17422d8bdb06e083e6a563d2c75d53
Karan Ahir <ahirkaran218@gmail.com>
2026-02-13 07:49:33 +0530

    remove unused file and init web app

---
+++ apps/web/app/login/page.tsx
@@ -0,0 +1,12 @@
+import Authentication from '../../lib/component/auth';
+import React from 'react';
+
+function Login() {
+  return (
+    <div>
+      <Authentication title={'Login'} />
+    </div>
+  );
+}
+
+export default Login;
